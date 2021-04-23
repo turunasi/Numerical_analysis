6a943e5efa9d98c491c4918b293317800b092283
Yuki Nishii <ynishii@diverta.co.jp>
2021-04-23 14:27:58 +0900

    fix

--- 3/1.py
+++ 3/1.py
@@ -1,16 +1,41 @@
-def main():
-    x_p = float(input("Input initial value:"))
-    c = 0
-    diff = 1
-    while diff != 0:
-        x = (x_p**2+1)/(2*x_p-1)
-        diff = abs(x-x_p)
-        x_p = x
-        c += 1
-        print("{0}:{1}".format(c,diff))
-    print("Number of trials:{}".format(c))
-    print("Limiting value:{}".format(x))
+import numpy as np
+from matplotlib import pyplot as plt
 
+def main():
+    a = float(input("Input a:"))
+    b = float(input("Input b:"))
+    x = []
+    y = []
+    count = 0
+    fig = plt.figure()
+    if f(a)*f(b) >= 0:
+        print("a or b is invalid value.\n")
+        return 1
+    while a != b:
+        c = (a+b)/2
+        s = f(a)*f(c)
+        if s < 0:
+            b = c
+        else:
+            a = c 
+        count += 1
+        x += [count]
+        y += [abs(a-b)]
+        print("{0}:{1}".format(count,a-b))
+    print("Number of trials:{}".format(c))
+    print("Limiting value:{}".format(a))
+    x = np.array(x)
+    y = np.array(y)
+    plt.plot(x, y, label='diff')
+    plt.title('Diff Graph')
+    plt.xlabel('Count')
+    plt.ylabel('Diff')
+    plt.legend()
+    plt.show()
+    fig.savefig("1.png")
+
+def f(x):
+    return x**2-x-1
 
 if __name__ == "__main__":
     main()
--- 3/2.py
+++ 3/2.py
@@ -1,19 +1,35 @@
 import math
+import numpy as np
+from matplotlib import pyplot as plt
 
 def main():
     x_p = 0
     x = float(input("Input initial value:"))
+    x_list = []
+    y_list = []
     c = 0
     diff = 1
+    fig = plt.figure()
     while diff != 0:
         x_n = x-(x-x_p)*(math.cos(x)-x**2)/(math.cos(x)-x**2-math.cos(x_p)+x_p**2)
         diff = abs(x_n-x)
         x_p = x
         x = x_n
         c += 1
+        x_list += [c]
+        y_list += [diff]
         print("{0}:{1}".format(c,diff))
     print("Number of trials:{}".format(c))
     print("Limiting value:{}".format(x_n))
+    x_list = np.array(x_list)
+    y_list = np.array(y_list)
+    plt.plot(x_list, y_list, label='diff')
+    plt.title('Diff Graph')
+    plt.xlabel('Count')
+    plt.ylabel('Diff')
+    plt.legend()
+    plt.show()
+    fig.savefig("2.png")
 
 if __name__ == "__main__":
     main()
